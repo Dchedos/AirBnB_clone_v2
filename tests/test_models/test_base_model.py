c504ca5d7bb2d2c375e0decd32f106f9e20b6ee0
Nchedo Okongwu <nchedookongwu@yahoo.com>
2023-03-19 04:54:04 -0700

    Update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -35,7 +35,7 @@ class test_basemodel(unittest.TestCase):
     def tearDown(self):
         try:
             os.remove('file.json')
-        except:
+        except ValueError:
             pass
 
     def test_default(self):
