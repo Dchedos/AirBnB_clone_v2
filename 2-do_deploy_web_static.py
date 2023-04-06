7ea172c74be3a97654e5f06387e850739689e2b7
Nchedo Okongwu <nchedookongwu@yahoo.com>
2023-04-06 05:52:14 -0700

    Task 2. Write a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['52.90.23.6', '52.207.96.101']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
