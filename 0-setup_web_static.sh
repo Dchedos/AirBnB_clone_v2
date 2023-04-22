e86a67658fcb3b176426776b4b9b45daeadad1d6
Nchedo Okongwu <nchedookongwu@yahoo.com>
2023-04-22 05:10:59 -0700

    Task 0. Write a Bash script that sets up your web servers for the deployment of web_static.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -25,5 +25,28 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 
 # set up nginx server configuration
-sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
-sudo service nginx restart
+SERVER=$(hostname)
+
+SERVER_CONFIG="server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        location / {
+                add_header X-Served-By '$SERVER';
+                try_files \$uri \$uri/ =404;
+        }
+
+	location /hbnb_static {
+                add_header X-Served-By '$SERVER';
+		alias /data/web_static/current;
+	}
+}"
+bash -c "echo -e '$SERVER_CONFIG' > /etc/nginx/sites-available/default"
+/etc/init.d/nginx restart
+
+exit 0
